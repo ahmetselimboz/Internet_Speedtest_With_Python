052fe4a8f9ed3e9dc8ee545ead98497dfa766951
ahmetselimboz <ahmetselimboz46@gmail.com>
2022-06-21 12:06:21 +0300

    Speedtest

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ import time
 
 st = speedtest.Speedtest()
 
-def humansize(nbytes):
-    suffixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
-    i = 0
-    while nbytes >= 1024 and i < len(suffixes)-1:
-        nbytes /= 1024.
-        i += 1
-    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
-    return '%s %s' % (f, suffixes[i])
+
 
 
 print("-------Internet Speedtest--------")
